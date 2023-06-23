cdecfa73ab8ac60b1cc1eb0c9f90a5a9f72e8653
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-23 15:00:17 +0200

    ex-09

---
+++ Exercises-9/Exercises-9/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Insert temperature in Celsius");
+
+string x = Console.ReadLine();
+
+int num = int.Parse(x);
+
+double result = (num * 1.8) + 32;
+
+Console.WriteLine($"{num} degrees Celsius are equivalent to {result} degrees Fahrenheit");
