382718eb59593db2e5ee51d5173ba63400fa2ccc
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-23 13:59:03 +0200

    ex-03

---
+++ Exercises-3/Exercises-3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insert number:");
+
+string input = Console.ReadLine();
+
+Console.WriteLine("Another one:");
+
+string input2 = Console.ReadLine();
+
+int sum = int.Parse(input) + int.Parse(input2);
+
+Console.WriteLine($"You provided {input} and {input2}. This is their sum {sum}");
