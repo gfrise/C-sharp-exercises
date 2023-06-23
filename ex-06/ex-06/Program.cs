2924d9b2a2111ad3e54f55ea209943cf4ca8a406
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-23 14:25:16 +0200

    ex-06

---
+++ ex-06/ex-06/Program.cs
@@ -0,0 +1,55 @@
+﻿class Animal
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+
+    public virtual void Speak()
+    {
+        Console.WriteLine($"{Name}, born {Age} years ago is speaking");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine($"{Name}, born {Age} years ago is howling");
+    }
+}
+
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine($"{Name}, born {Age} years ago is meowing");
+    }
+}
+
+class Bird : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine($"{Name}, born {Age} years ago is tweeting");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args )
+    {
+        Dog dog = new Dog();
+        dog.Name = "Pluto";
+        dog.Age = 13;
+        dog.Speak();
+
+        Cat cat = new Cat();
+        cat.Name = "Gambadilegno";
+        cat.Age = 3;
+        cat.Speak();
+
+        Bird bird = new Bird();
+        bird.Name = "Tuco";
+        bird.Age = 5;
+        bird.Speak();
+    }
+}
