f2f7d7a727c9b179f0bf30a3149425dca41afae1
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-23 14:04:28 +0200

    ex-04

---
+++ Exercise-4/Exercise-4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insert some text:");
+
+string input = Console.ReadLine();
+
+char[] toReverse = input.ToCharArray();
+
+Array.Reverse(toReverse);
+
+string reversed  = new string(toReverse);
+
+Console.WriteLine(reversed);   
