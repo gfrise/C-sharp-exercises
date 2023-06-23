a168c76a71367c8ed713564969881a4a12e97c20
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-23 15:25:20 +0200

    ex-10

---
+++ Exercises-10/Exercises-10/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Press 1 to convert from Fahrenheit to Celsius. Press 2 to do the opposite");
+
+int choice = int.Parse(Console.ReadLine());
+
+int num = 0;
+
+if (choice != 1 && choice != 2)
+{
+    Console.WriteLine("Not a valid input");
+    Environment.Exit(1);
+} else 
+{ 
+    Console.WriteLine("Insert a number");
+    num = int.Parse(Console.ReadLine());    
+}
+
+if (choice == 1)
+{
+    double result = (num - 32) * 0.5556;
+    Console.WriteLine($"{num} degrees Fahrenheit equals {result} degrees Celsius");
+} else
+{
+    double result = (num * 1.8) + 32;
+    Console.WriteLine($"{num} degrees Celsius equals {result} degrees Fahrenheit");
+}
