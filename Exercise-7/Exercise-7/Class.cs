dd01f083fb76351d0e4c4a20217133ea244dd707
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-25 17:40:43 +0200

    ex-07

---
+++ Exercise-7/Exercise-7/Class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    public class Class
+    {
+            public int publicField;
+
+            private int privateField;
+
+        protected int protectedField;
+
+            internal int internalField;
+
+        public Class()
+        {
+            publicField = 1;
+            privateField = 2;
+            protectedField = 3; 
+            internalField = 4;
+        }
+
+        public void PublicMethod()
+            {
+                Console.WriteLine("public method.");
+
+            }
+
+            internal void InternalMethod()
+            {
+                Console.WriteLine("internal method.");
+
+            }
+
+        public void AccessPrivate()
+        {
+            Console.WriteLine($"This is the only way to access private fields, which btw in this case is equale to{privateField}");
+            PrivateMethod();
+        }
+            protected void ProtectedMethod()
+            {
+                Console.WriteLine("protected method.");
+
+            }
+
+            private void PrivateMethod()
+            {
+                Console.WriteLine("private method.");
+
+            }
+        
+    }
+}
---
+++ Exercise-7/Exercise-7/Program.cs
@@ -0,0 +1,43 @@
+﻿using Exercise_7;
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Class myclass = new Class();
+
+        //These can be accessed directly
+
+        Console.WriteLine(myclass.publicField);
+        Console.WriteLine(myclass.internalField);
+
+        myclass.PublicMethod();
+        myclass.InternalMethod();
+
+    //  These cause an error
+
+     //   myclass.ProtectedMethod();
+     //   myclass.PrivateMethod();
+     //  Console.WriteLine(myclass.privateField);
+     // Console.WriteLine(myclass.protectedField);
+
+    //  the protected field and method are available like this
+        ClassInheriting otherclass = new ClassInheriting();
+        otherclass.Access();
+
+        // This makes private fields and methods available
+        myclass.AccessPrivate();
+    }
+}
+
+public class ClassInheriting :  Class
+{
+    public void Access()
+    {
+        protectedField = 3;
+        ProtectedMethod();
+        Console.WriteLine($"Accessed protected field through child class: {protectedField}");
+    }
+
+}
