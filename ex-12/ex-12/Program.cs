9284ca2c96bec9fe94c1828a18538566e4fa980c
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-28 11:19:01 +0200

    ex-12

---
+++ ex-12/ex-12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+ArrayList arrayList = new ArrayList();
+
+arrayList.Add(17);
+arrayList.Add("Testing");
+arrayList.Add(3.14);
+arrayList.Add(false);
+
+Console.WriteLine("This is inside the arraylist:");
+foreach (var el in arrayList)
+{
+    Console.WriteLine(el);
+}
