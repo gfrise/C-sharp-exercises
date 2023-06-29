26e1f509f6f66fda6d0775026256c529f49cc3d1
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-29 12:56:57 +0200

    ex-17

---
+++ Ex-17/Ex-17/Program.cs
@@ -0,0 +1,19 @@
+﻿HashSet<string> ensemble = new HashSet<string>();
+
+ensemble.Add("il fuoco");
+ensemble.Add("el fuego");
+ensemble.Add("le feu");
+ensemble.Add("ignis");
+ensemble.Add("pyros");
+ensemble.Add("ogon");
+ensemble.Add("fire");
+ensemble.Add("FIRE");
+
+Console.WriteLine($"The ensemble hashSet contains {ensemble.Count} element");
+
+foreach(var x in ensemble)
+{
+    Console.WriteLine(x);
+}
+
+Console.WriteLine("Capitalized letters are considered different");
