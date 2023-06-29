e6a58d8d1ce26ddf28449767fa0f83662b0e67b5
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-29 13:27:00 +0200

    ex-18

---
+++ ex-18/ex-18/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> numbers = new List<int>();
+
+void NumberAdder()
+{
+    Random random = new Random();
+
+    for (int i = 0; i <= 1000; i++)
+    {
+        numbers.Add(random.Next());
+    }
+}
+NumberAdder();
+
+Console.WriteLine($"The smallest is {numbers.Min()}");
+Console.WriteLine($"The biggest is {numbers.Max()}");
