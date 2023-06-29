f72e492e0af933e58673225f100eb57eb9753317
gfrise <104461249+gfrise@users.noreply.github.com>
2023-06-29 12:43:39 +0200

    Ex-16

---
+++ Ex-16/Ex-16/Program.cs
@@ -0,0 +1,31 @@
+﻿// /.output.txt
+Console.WriteLine("Insert file path");
+string path = Console.ReadLine();
+
+if (!File.Exists(path))
+{
+    Console.WriteLine($"{path} is not valid");
+    return;
+}
+
+string content = File.ReadAllText(path);
+
+Dictionary<string, int> count =  new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+string[] words = content.Split(new char[] { ' ', '\t', '\n', '\r', ',', '.', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var keyword in words) {
+
+    if (count.ContainsKey(keyword))
+    {
+        count[keyword]++;
+    } else
+    {
+        count[keyword] = 1;
+    }
+}
+
+foreach (var x in count)
+{
+    Console.WriteLine($"{x.Key} {x.Value}");
+}
