930304eddcf67ad610b3f1a2aa03299e167aae2c
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-04 18:59:39 +0200

    Ex23

---
+++ ex23/ex23/Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex23
+{
+    public class Filter
+    {
+        public static IEnumerable<int> GetOdds(IEnumerable<int> nums)
+        {
+            return nums.Where(i => i % 2 != 0).OrderBy(i => i);
+        }
+        public static IEnumerable<int> GetOddsReversed(IEnumerable<int> nums)
+        {
+            return nums.Where(i => i % 2 != 0).OrderByDescending(i => i);
+        }
+    }
+}
---
+++ ex23/ex23/Program.cs
@@ -0,0 +1,23 @@
+﻿using ex23;
+
+Random random = new Random();
+var range = Enumerable
+    .Range(0, 1001)
+    .Select(i => random.Next(0, 999));
+
+Console.WriteLine("Press 1 to generate list of low to high odd numbers. Press 2 to see high to low.");
+int input = int.Parse(Console.ReadLine());
+
+if (input == 1)
+{
+    Filter.GetOdds(range).ToList().ForEach(i => Console.WriteLine(i));
+
+}
+else if (input == 2)
+{
+    Filter.GetOddsReversed(range).ToList().ForEach(i => Console.WriteLine(i));
+}
+else
+{
+    Console.WriteLine("Not allowed");
+}
