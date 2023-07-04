cda9adaa6f3f6c1aac60570f8add319bf56c3bd5
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-04 18:32:26 +0200

    Ex-21

---
+++ Ex-21/Ex-21/Program.cs
@@ -0,0 +1,18 @@
+﻿Random random = new Random();
+var range = Enumerable
+    .Range(0, 1001)
+    .Select(i => random.Next(0, 999));
+
+IEnumerable<int> GetOdds(IEnumerable<int> nums)
+{
+    return (from i in nums
+            where i % 2 != 0
+            orderby i
+            select i).ToList();
+
+}
+
+foreach(var i in GetOdds(range))
+{
+    Console.WriteLine(i);
+}
