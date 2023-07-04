80f9c92e3fc121b67dc0ef21d2a543657f7a3e05
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-04 19:37:40 +0200

    Ex-24

---
+++ Ex24/Ex24/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ex24;
+using System.Collections.Immutable;
+
+var rdm = new Random();
+var range = Enumerable
+    .Range(0, 15)
+    .Select(n => rdm.Next(0, 200));
+IEnumerable<int> NumberLength(IEnumerable<int> nums)
+{
+    CustomRanker ranker = new CustomRanker();
+    var x = nums.ToList();
+    x.Sort(ranker);
+
+    return x;
+}
+
+foreach(var i in NumberLength(range))
+{
+    Console.WriteLine(i);
+}
