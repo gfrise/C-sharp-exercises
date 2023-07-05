de6ca2f97fa1365d98dd96e015dbf28af3869301
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-05 17:22:50 +0200

    Ex28

---
+++ Ex28/Ex28/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter some numbers separated by commas:");
+string input = Console.ReadLine();
+
+var nums = input
+    .Split(',')
+    .Select(int.Parse)
+    .ToList();
+
+bool AllEven = nums.All(x => x % 2 == 0);
+bool AnyOdd = nums.Any(x => x % 2 != 0);
+
+Console.WriteLine($"It's {AllEven} that are all even, and {AnyOdd} that there's at least an odd one");
