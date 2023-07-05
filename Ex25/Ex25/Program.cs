7ce9d0771e71bf39e95c136d684361f68657cb28
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-05 16:45:21 +0200

    Ex25

---
+++ Ex25/Ex25/Program.cs
@@ -0,0 +1,12 @@
+﻿var nums = new List<double>
+{
+    3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9,
+};
+
+var sum = nums.Sum();
+var avg = nums.Average();
+var length = nums.Count();
+
+Console.WriteLine($"Sum: {sum}, Average: {avg}, Number of elements: {length}");
+
+
