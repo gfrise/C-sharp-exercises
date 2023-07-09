59b2781116f8a8f8e71b00e53ace86d76db6b06f
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-09 16:39:45 +0200

    Ex30

---
+++ Ex30/Ex30/NumberMetaData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex30
+{
+    public class NumberMetaData
+    {
+        public int OriginalNum { get; set; }
+        public bool isEven { get; set; }
+
+        public int NumberOfCharacters { get; set; }
+    }
+}
---
+++ Ex30/Ex30/Program.cs
@@ -0,0 +1,18 @@
+﻿using Ex30;
+
+Random random = new Random();
+
+var range = Enumerable
+    .Range(0, 100)
+    .Select(i => random.Next(0, 1999));
+
+var meta = range.Select(i => new NumberMetaData
+{   OriginalNum = i,
+    isEven = i % 2 == 0,
+    NumberOfCharacters = i.ToString().Length,
+}).ToList();
+
+foreach(var i in meta)
+{
+    Console.WriteLine($"{i.OriginalNum}. It's {i.isEven} that it's even and its length is {i.NumberOfCharacters}");
+}
