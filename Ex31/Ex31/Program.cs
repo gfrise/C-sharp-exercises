f85617aa9f9e1f0d2f46baf886010465da30e4d7
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-11 15:40:02 +0200

    Ex31

---
+++ Ex31/Ex31/Program.cs
@@ -0,0 +1,53 @@
+﻿var intStack = new MyQueue<int>();
+intStack.Add(1);
+intStack.Add(2);
+intStack.Add(3);
+intStack.Remove();
+
+var stringStack = new MyQueue<string>();
+stringStack.Add("Bob");
+stringStack.Add("Alice");
+stringStack.Add("Tim");
+stringStack.Remove();
+
+var x = stringStack.TakeALook();
+var y = intStack.TakeALook();
+Console.WriteLine(y);
+
+class MyQueue<T>
+{
+    private readonly Queue<T> _queue;
+
+    public MyQueue()
+    {
+            _queue = new Queue<T>();
+    }
+
+    public void Add(T el)
+    {
+        _queue.Enqueue(el);
+    }
+
+    public void Remove()
+    {
+        if (_queue.Count != 0)
+        {
+           _queue.Dequeue();
+
+        } else
+        {
+            throw new InvalidOperationException("empty queue");
+        }
+    }
+
+    public T TakeALook()
+    {
+        if (_queue.Count != 0)
+        {
+            return _queue.Peek();
+        } else
+        {
+            throw new InvalidOperationException("empty queue");
+        }
+    }
+}
