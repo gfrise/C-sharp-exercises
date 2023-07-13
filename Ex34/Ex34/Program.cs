20389c02a1560ffb52d17800b4ea0a75b569f1f7
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-13 21:24:09 +0200

    Ex34

---
+++ Ex34/Ex34/Program.cs
@@ -0,0 +1,13 @@
+﻿using Ex34;
+
+var intStack = new MyQueue<int>();
+intStack.Add(1);
+intStack.Add(2);
+intStack.Add(3);
+intStack.Remove();
+
+var stringStack = new MyQueue<string>();
+stringStack.Add("Bob");
+stringStack.Add("Alice");
+stringStack.Add("Tim");
+stringStack.Remove();
