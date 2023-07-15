30fadeced1367f15cb1214603bdb603fc42f2323
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-15 17:26:02 +0200

    Ex36

---
+++ Ex36/Ex36/MyQueue.cs
@@ -0,0 +1,53 @@
+﻿namespace Ex36
+{
+    public interface IQueue<T>
+    {
+        void Add(T el);
+        void Remove();
+        T TakeALook();
+    }
+    public class MyQueue<T> : IQueue<T>
+    {
+        private readonly Queue<T> _queue;
+
+        public MyQueue()
+        {
+            _queue = new Queue<T>();
+        }
+
+        public void Add(T el)
+        {
+            _queue.Enqueue(el);
+        }
+
+        public void Remove()
+        {
+            if (_queue.Count != 0)
+            {
+                _queue.Dequeue();
+
+            }
+            else
+            {
+                throw new InvalidOperationException("empty queue");
+            }
+        }
+
+        public T TakeALook()
+        {
+            if (_queue.Count != 0)
+            {
+                return _queue.Peek();
+            }
+            else
+            {
+                throw new InvalidOperationException("empty queue");
+            }
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _queue;
+        }
+    }
+}
