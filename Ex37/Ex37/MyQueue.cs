df602721afbd500fc748a3a0f5de2f22186b7cf5
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-15 17:37:06 +0200

    Ex37

---
+++ Ex37/Ex37/MyQueue.cs
@@ -0,0 +1,57 @@
+﻿namespace Ex37
+{
+    public class MyQueue<T>
+    {
+        private readonly Queue<T> _queue;
+
+        public MyQueue()
+        {
+            _queue = new Queue<T>();
+        }
+
+        public void Add(T el)
+        {
+            _queue.Enqueue(el);
+        }
+
+        public void Remove()
+        {
+            if (_queue.Count != 0)
+            {
+                _queue.Dequeue();
+
+            }
+            else
+            {
+                throw new InvalidOperationException("empty queue");
+            }
+        }
+
+        public T TakeALook()
+        {
+            if (_queue.Count != 0)
+            {
+                return _queue.Peek();
+            }
+            else
+            {
+                throw new InvalidOperationException("empty queue");
+            }
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _queue;
+        }
+    }
+
+    public class IntQueue : MyQueue<int>
+    {
+
+    }
+
+    public class StringQueue : MyQueue<string>
+    {
+
+    }
+}
