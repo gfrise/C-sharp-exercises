216a69063d565b01d37288a3589e43784365cccb
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-15 17:59:37 +0200

    Ex38

---
+++ Ex38/Ex38/MyQueue.cs
@@ -0,0 +1,75 @@
+﻿namespace Ex38
+{
+    public interface IAdd<T>
+    {
+        void Add(T el);
+    }
+
+    public interface IRemove
+    {
+        void Remove();
+    }
+
+    public interface IPeek<T>
+    {
+        T TakeALook();
+    }
+    public interface IGet<T>
+    {
+        IEnumerable<T> Get();
+    }
+    public class MyQueue<T> : IAdd<T>, IRemove, IPeek<T>, IGet<T>
+    {
+        private readonly Queue<T> _queue;
+
+        public MyQueue()
+        {
+            _queue = new Queue<T>();
+        }
+
+        public void Add(T el)
+        {
+            _queue.Enqueue(el);
+        }
+
+        public void Remove()
+        {
+            if (_queue.Count != 0)
+            {
+                _queue.Dequeue();
+
+            }
+            else
+            {
+                throw new InvalidOperationException("empty queue");
+            }
+        }
+
+        public T TakeALook()
+        {
+            if (_queue.Count != 0)
+            {
+                return _queue.Peek();
+            }
+            else
+            {
+                throw new InvalidOperationException("empty queue");
+            }
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _queue;
+        }
+    }
+
+    public class IntQueue : MyQueue<int>
+    {
+
+    }
+
+    public class StringQueue : MyQueue<string>
+    {
+
+    }
+}
---
+++ Ex38/Ex38/Program.cs
@@ -0,0 +1,23 @@
+﻿using Ex38;
+
+var intStack = new IntQueue();
+intStack.Add(1);
+intStack.Add(2);
+intStack.Add(3);
+intStack.Remove();
+
+var stringStack = new StringQueue();
+stringStack.Add("Bob");
+stringStack.Add("Alice");
+stringStack.Add("Tim");
+stringStack.Remove();
+
+foreach (var i in intStack.Get())
+{
+    Console.WriteLine($"{i}");
+}
+
+foreach (var i in stringStack.Get())
+{
+    Console.WriteLine($"{i}");
+}
