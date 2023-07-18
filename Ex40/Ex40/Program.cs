a212fd4bf769b9a76624cb07f8d20b416b1c1698
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-18 15:50:21 +0200

    Ex40

--- Ex40/Ex40/Program.cs
+++ Ex40/Ex40/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Enter some content");
 string content = Console.ReadLine();
 
- File.WriteAllTextAsync(path, content);
+await File.WriteAllTextAsync(path, content);
 
 var read = await File.ReadAllTextAsync(path);
 Console.WriteLine($"{read}");
