6c54a3823573b3bc27aed599f7dc10652d5f3e6f
gfrise <104461249+gfrise@users.noreply.github.com>
2023-07-21 23:42:33 +0200

    Ex41

---
+++ Ex41/Ex41/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Insert an url");
+string url = Console.ReadLine();
+
+Console.WriteLine(await new Fetch().From(url));
+public class Fetch
+{
+    public async Task<string> From(string i)
+    {
+        var res = await new HttpClient().GetAsync(new Uri(i));
+
+        foreach(var h in res.Headers)
+        {
+            Console.WriteLine($"-{h.Key}= {h.Value}");
+        }
+
+        Console.WriteLine(await res.Content.ReadAsStringAsync());
+
+        return "";
+    }
+}
